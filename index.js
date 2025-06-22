8afbdf6efea6366d66773e4f040b4de8953bce43
bilu abdi <biluabdi91@gmail.com>
2025-06-22 16:40:35 +0300

    fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email,
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(data => {
+      const newId = document.createElement("p");
+      newId.textContent = `ID: ${data.id}`;
+      document.body.appendChild(newId);
+    })
+    .catch(error => {
+      const errorMsg = document.createElement("p");
+      errorMsg.textContent = error.message;
+      document.body.appendChild(errorMsg);
+    });
+}
